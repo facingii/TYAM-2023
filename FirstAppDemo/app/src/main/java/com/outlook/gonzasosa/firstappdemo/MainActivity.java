d00b5e134552e8a3477d69ff6396de47006e8810
Gonzalo Sosa <gonzasosa@outlook.com>
2023-09-08 17:05:44 -0600

    Persisting Widgets state - Basic way

--- FirstAppDemo/app/src/main/java/com/outlook/gonzasosa/firstappdemo/MainActivity.java
+++ FirstAppDemo/app/src/main/java/com/outlook/gonzasosa/firstappdemo/MainActivity.java
@@ -1,16 +1,29 @@
 package com.outlook.gonzasosa.firstappdemo;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String NAME = "name";
+    private final static String LASTNAME  = "lastname";
+    private final static String PHONE  = "phone";
+    private final static String IMAGELOADED = "loaded";
+
+    EditText edtName;
+    EditText edtLastname;
+    EditText edtPhone;
+
+    ImageView imageView;
+    boolean imageLoaded;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,47 +31,50 @@ public class MainActivity extends AppCompatActivity {
         Log.i ("FIEE", "OnCreate");
         setContentView (R.layout.activity_main);
 
-        EditText edtName = findViewById (R.id.edtName);
+        edtName = findViewById (R.id.edtName);
+        edtLastname = findViewById (R.id.edtLastname);
+        edtPhone = findViewById (R.id.edtPhone);
 
         Button btnSend = findViewById (R.id.btnSend);
         btnSend.setOnClickListener (view -> Toast.makeText (this, edtName.getText().toString(), Toast.LENGTH_LONG ).show());
 
-        ImageView imageView = findViewById (R.id.ivPicture);
+        imageView = findViewById (R.id.ivPicture);
 
         Button btnCamera = findViewById(R.id.btnOpenCamera);
         btnCamera.setOnClickListener (view -> {
             imageView.setImageResource (R.mipmap.ic_launcher_round);
+            imageLoaded = true;
         });
 
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i ("FIEE", "OnStart");
+    protected void onSaveInstanceState (@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString (NAME, edtName.getText().toString());
+        outState.putString (LASTNAME, edtLastname.getText().toString());
+        outState.putString (PHONE, edtPhone.getText().toString());
+        outState.putBoolean (IMAGELOADED, imageLoaded);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i ("FIEE", "OnResume");
-    }
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i ("FIEE", "OnPause");
-    }
+        String name = savedInstanceState.getString (NAME, "");
+        edtName.setText (name);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i ("FIEE", "OnStop");
-    }
+        String lastName = savedInstanceState.getString (LASTNAME, "");
+        edtLastname.setText (lastName);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i ("FIEE", "OnDestroy");
+        String phone = savedInstanceState.getString (PHONE, "");
+        edtPhone.setText (phone);
+
+        Boolean imageLoaded = savedInstanceState.getBoolean (IMAGELOADED, false);
+        if (imageLoaded) {
+            imageView.setImageResource (R.mipmap.ic_launcher_round);
+            imageLoaded = true;
+        }
     }
 }
